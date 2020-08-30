f4be549ac90dda65449457c7a1c3aa6ca249e7e0
kanika varshney <kanika.varshney_cs19@gla.ac.in>
2020-08-30 22:12:03 +0530

    final commit

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -5,6 +5,60 @@
  *  File Name : Book.java
  * */
 package definitions;
-
+import java.util.Objects;
 public class Book {
+    private String bookName;
+    private String bookAuthorName;
+    private String isbnNumber;
+
+    public Book() {
+        this.bookName = "The Discovery of India";
+        this.bookAuthorName = "Jawahar Lal Nehru";
+        this.isbnNumber = "12536142";
+        this.bookName = null;
+        this.bookAuthorName = null;
+        this.isbnNumber = null;
+    }
+
+    public Book(String bookName, String bookAuthorName, String isbnNumber) {
+        this.bookName = bookName;
+        this.bookAuthorName = bookAuthorName;
+        this.isbnNumber = isbnNumber;
+    }
+    public String getBookName() {
+        return bookName;
+    }
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+    public String getBookAuthorName() {
+        return bookAuthorName;
+    }
+    public void setBookAuthorName(String bookAuthorName) {
+        this.bookAuthorName = bookAuthorName;
+    }
+    public String getIsbnNumber() {
+        return isbnNumber;
+    }
+    public void setIsbnNumber(String isbnNumber) {
+        this.isbnNumber = isbnNumber;
+    }
+    public String toString() {
+        return "Book Name" + getBookName() + "," +
+                "Authors Name" + getBookAuthorName() + "," +
+                "isbnNumber" + getIsbnNumber() + ".";
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(bookName, book.bookName) &&
+                Objects.equals(bookAuthorName, book.bookAuthorName) &&
+                Objects.equals(isbnNumber, book.isbnNumber);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookName, bookAuthorName, isbnNumber);
+    }
 }
